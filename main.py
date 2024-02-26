b9b6df40babb1a866e04093730f072a865649e2f
ripEnderdragon <naufalvramadhan8@gmail.com>
2024-02-26 17:51:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            }
+while True:
+    meme_dict = {
+                "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+                "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+                }
 
-word = input("Ketik kata yang tidak Kamu mengerti:") .upper()
+    word = input("Ketik kata yang tidak Kamu mengerti:") .upper()
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Maaf, saat ini kami tidak memiliki kamus untuk kata ini namun kami akan terus memperluas kamus kami")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Maaf, saat ini kami tidak memiliki kamus untuk kata ini namun kami akan terus memperluas kamus kami ;D")
